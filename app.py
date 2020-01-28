76627f008f5b63d9fdb225154ba0df09333d0784
shubhamrocks888 <shubhamrocks888@gmail.com>
2020-01-28 16:26:36 +0530

    returning a list of stores

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask  import Flask
+from flask  import Flask,jsonify
 
 app = Flask(__name__)
 
@@ -18,9 +18,9 @@ def get_store(name):
 
 @app.route('/store')
 def get_stores():
-    pass
+    return jsonify({"stores":stores})
 
-@app.route('/store/<string:name/item>',methods=['POST'])
+@app.route('/store/<string:name>/item',methods=['POST'])
 def create_item_in_store(name):
     pass
 
